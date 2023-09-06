7e455498737b80f100e681d2c8bde9fea3d4c7cf
Obarajames <omwoyoobara@gmail.com>
2023-09-06 15:05:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- the font awsome link -->
+    <script src="https://kit.fontawesome.com/deb49aa60e.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- i think u all know that a php file ends with a ".php" -->
+    <nav>
+        <div class="nav-left">
+            <!-- the name of the web/logo -->
+            <i class="fas fa-bars bar"></i>
+            <div class="title">
+            <i class="fas fa-flag flag"></i>
+            <h1>Simple Servey</h1>
+            </div>
+        </div>
+        <div class="nav-center">
+            <div class="search">
+            <i class="fas fa-search searchicon"></i> 
+            <input type="text" placeholder="Search">
+            </div>
+
+        </div>
+        <div class="nav-right">
+            <div class="button">
+            <i class="fas fa-cog btn"></i>
+            <i class="fas fa-bars btn"></i>
+            <i class="fas fa-user btn"></i>
+            </div>
+        </div>
+    </nav>
+    <!-- the end of the navigaton bar  -->
+    <div class="container">
+        <div class="left-bar">
+            
+                <div class="icon-title">
+                <i class="fas fa-lightbulb tresh"></i>
+                <div class="lables">
+                    Notes
+                </div>
+                </div>
+                <div class="icon-title">
+                <i class="fas fa-bell tresh"></i>
+                <div class="lables">
+                    Reminder
+                </div>
+                </div>
+
+                <div class="icon-title">
+                <i class="fas fa-pencil-alt tresh"></i>
+                <div class="lables">
+                    Edit Lables
+                </div>
+                </div>
+
+                <div class="icon-title">
+                <i class="fas fa-archive tresh"></i>
+                <div class="lables">
+                    Achives
+                </div>
+                </div>
+                <div class="icon-title">
+                <i class="fas fa-trash tresh"></i>
+                <div class="lables">
+                  Bin
+                </div>
+                </div>
+            
+
+        </div>
+        <div class="right-bar">
+
+        </div>
+    </div>
+</body>
+</html>
